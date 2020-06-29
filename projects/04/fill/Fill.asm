d4cd9fe6bd6a87f78d8bc1d51ac7087ee1fca60a
Jan-Preben Mossin <jan.preben.mossin@vimond.com>
2020-06-29 22:23:36 +0200

    04 / fill.asm off by one bug

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,7 +6,7 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
@@ -32,7 +32,7 @@
 	@WRITE
 	0;JMP
 
-	
+
 (WRITE)
 	// compute adr to write: screen start adr + current index
 	@SCREEN
@@ -53,7 +53,7 @@
 	@R1
 	MD=M-1
 	@START_LOOP
-	D;JEQ
+	D;JLT
 	@NEXT_PIXEL
 	0;JMP
-	
+
